72a738e33d6cde9632bbf5c172646c9337afcb8a
zainasr <mynameiszainmalik@gmail.com>
2025-11-30 18:59:54 +0500

    first

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export function greet(name) {
+    return `Hello, ${name}!`;
+  }
+  
+  console.log(greet("World"));
+  
---
+++ tests/index.test.js
@@ -0,0 +1,6 @@
+import test from 'node:test';
+import assert from 'node:assert';
+
+test('simple math test', () => {
+  assert.equal(2 + 2, 4);
+});
